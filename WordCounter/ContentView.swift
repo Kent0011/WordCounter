71518d772623acc6854b70541d065a47fc00be4c
Kento Yamamoto <key7135g@gmail.com>
2024-08-23 18:19:30 +0900

    最初のコミット

--- WordCounter/ContentView.swift
+++ WordCounter/ContentView.swift
@@ -8,13 +8,26 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var inputText = ""
+    @State var textnumber = 0
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack() {
+            TextField("テキストを入力", text:$inputText)
+//                .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/)
+            HStack(spacing:20){
+                Button("Reset"){
+                    inputText = ""
+                }
+                Button("Count") {
+                    textnumber = inputText.count
+                }
+            }
+            Text("文字数 : \(textnumber)")
+                .padding()
+            
         }
+        .frame(minWidth: 200, maxWidth: .infinity, minHeight: 100, maxHeight: .infinity)
+        
         .padding()
     }
 }
