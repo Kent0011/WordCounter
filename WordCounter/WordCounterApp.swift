f844085d4204d655af0dde2e8acf323ba32460ac
Kento Yamamoto <key7135g@gmail.com>
2024-08-23 13:29:50 +0900

    Initial Commit

---
+++ WordCounter/WordCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordCounterApp.swift
+//  WordCounter
+//
+//  Created by Kent Yamamoto on 2024/08/23.
+//
+
+import SwiftUI
+
+@main
+struct WordCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
